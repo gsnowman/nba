093f273ce12afd991a6f319696939a75ccf6d326
Greg Snow <greg.snow@chicagotrading.com>
2015-10-31 15:58:18 -0500

    adding update script - to be cronned and run daily

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR=$(cd $(dirname $0) && pwd)
+cd "${DIR}"
+
+export RUBYLIB=$RUBYLIB:"${DIR}/ruby"
+
+DATE=$(ruby $MY_HOME/fmtdate.rb b1)
+FILE="log/${DATE}.log"
+
+WEEK_NUM=1
+
+echo "Fetching games for yesterday..."            | tee -a "${FILE}.log"
+echo "==========================================" | tee -a "${FILE}.log"
+ruby ruby/fetch_games.rb                          | tee -a "${FILE}.log"
+
+echo "Updating owned status..."                   | tee -a "${FILE}.log"
+echo "==========================================" | tee -a "${FILE}.log"
+ruby ruby/update_owned_status.rb                  | tee -a "${FILE}.log"
+
+echo "Generating week stats, week ${WEEK_NUM}..." | tee -a "${FILE}.log"
+echo "==========================================" | tee -a "${FILE}.log"
+ruby ruby/generate_week_stats.rb "${WEEK_NUM}"    | tee -a "${FILE}.log"
+
